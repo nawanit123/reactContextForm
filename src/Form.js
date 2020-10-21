a12df6ed09f61110159b27208ddeef45a3c14e19
Nawanit <sharma.nawanit123@gmail.com>
2020-10-21 15:44:04 +0530

    Added formComponent

---
+++ src/Form.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Avatar } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { CssBaseLine } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
+import { FormControlLabel } from '@material-ui/core';
+import { Checkbox } from '@material-ui/core';
+import { Input } from '@material-ui/core';
+import { InputLabel } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { LockOutlined as LockOutlinedIcon } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/formStyles';
+
+class Form extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <main className={classes.main}>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.Avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Sign In</Typography>
+          <Select value="english">
+            <MenuItem value="english">English</MenuItem>
+            <MenuItem value="french">French</MenuItem>
+            <MenuItem value="spanish">Spanish</MenuItem>
+          </Select>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Input id="email" name="email" autofocus />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input id="password" name="password" autofocus />
+            </FormControl>
+            <FormControlLabel
+              label="Remember Me"
+              control={
+                <Checkbox
+                  // value=""
+                  // checked={}
+                  // onChange={}
+                  color="primary"
+                />
+              }
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              type="Submit"
+              fullWidth
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </Paper>
+      </main>
+    );
+  }
+}
+
+export default withStyles(styles)(Form);
